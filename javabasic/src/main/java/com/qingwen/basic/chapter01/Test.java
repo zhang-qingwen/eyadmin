0e55018f078861d8ce9d0c4ccaafd6065d2ff216
zhangqingwen <zhangqingwen@outlook.com>
2022-09-20 15:05:59 +0800

    second commit

--- javabasic/src/main/java/com/qingwen/basic/chapter01/Test.java
+++ javabasic/src/main/java/com/qingwen/basic/chapter01/Test.java
@@ -2,6 +2,7 @@ package com.qingwen.basic.chapter01;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, git!!");
+        System.out.println("Hello, git1");
+        System.out.println("Hello, git2");
     }
 }
